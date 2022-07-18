4844d2923c6aaffe6857511e3e81ec68762a0d95
ciri-ryan <cirijaye@yahoo.com>
2022-07-18 08:56:03 -0700

    reducers, api, and try with func

---
+++ src/components/EPostList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EPostList = () => {
+  return (
+    <div>
+      Functional Post List with useEffect
+    </div>
+  )
+}
+
+export default EPostList
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions'
 
  class PostList extends React.Component {
+
+  componentDidMount(){
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
@@ -9,4 +16,6 @@ import React from 'react'
     )
   }
 }
-export default PostList;
+
+
+export default connect(null, { fetchPosts })(PostList);
